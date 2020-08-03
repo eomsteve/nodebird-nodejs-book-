d22ffc9107c8c25de2a626d5830346da7b6816d6
sheom <mingyov0807@gmail.com>
2020-08-03 10:20:55 +0900

    error fix for nodemon : es6 not allowed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import session from 'session';
-import flash from 'flash';
+const express =  require('express');;
+const cookieParser = require('cookie-parser');
+const morgan =require('morgan');
+const path =require('path');
+const session =require('express-session');
+const flash =require('connect-flash');
 require('dotenv').config();
 
 
-const pageRouter = require('./routes/page');
+const pageRouter = require('./routes/page.js');
 
 const app =express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave:false,
     saveUninitialized:false,
-    secret:'nodebirdsecret',
+    secret:process.env.COOKIE_SECRET,
     cookie:{
         httpOnly:true,
         secure:false,
--- routes/page.js
+++ routes/page.js
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 const router = express.Router();
 router.get('/profile',(req,res)=>{
     res.render('profile',{title:"내 정보 - node bird",user:null});
@@ -6,8 +6,12 @@ router.get('/profile',(req,res)=>{
 });
 
 router.get('/join',(req,res)=>{
-    res.render('main',{title:"nodebird",twits:[],user:null,loginError:req.flash('loginError')});
+    res.render('main',{title:"회원가입 - nodebird",user:null,loginError:req.flash('loginError')});
 
 })
 
+router.get('/',(req,res,next)=>{
+    res.render('main',{title:"nodebird",twits:[],user:null,loginError:req.flash('loginError')});
+
+})
 module.exports = router;
