0865002eaf55dce41f129a2d072b18aff1a4ace8
sheom <mingyov0807@gmail.com>
2020-08-05 12:26:54 +0900

    hashtag error fixed

--- routes/post.js
+++ routes/post.js
@@ -3,73 +3,78 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const {Post , Hashtag, User} = require('../models');
-const {isLoggedIn} =require('./middlewares.js');
+const { Post, Hashtag, User } = require('../models');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-fs.readdir('uploads',(error)=>{
-    if(error){
-        console.error('uploads폴더가 없어 upload폴더를 생성합니다.');
-        fs.mkdirSync('uploads');
-    }
+fs.readdir('uploads', (error) => {
+  if (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+  }
 });
 
 const upload = multer({
-    storage:multer.diskStorage({
-        destination(req,file,cb){
-            const ext = path.extname(file.originalname);
-            cb(null,path.basename(file.originalname,ext)+Date.now()+ext);
-        }
-    }),
-    limits:{filesize:5*1024*1024},
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` });
 });
 
 const upload2 = multer();
-router.post('/',isLoggedIn,upload2.none(),async(req,res,next)=>{
-    try{
-        const post = await Post.create({
-            content:req.body.content,
-            img:req.body.url,
-            userId:req.user.id,
-        });
-        const hashtags = req.body.content.match(/#[^s#]*/g);
-        if(hashtags){
-            const result = await Promise.all(hashtags.map(tag => hashtag.findOrCreate({
-                where:{title:tag.slice(1).toLowerCase()},
-            })));
-            await post.addHashtags(result.map(r=>r[0]));
-        }
-        res.redirect('/');
-
-    }catch(error){
-        console.error(error);
-        next(error);
-        
+router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      userId: req.user.id,
+    });
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map(tag => Hashtag.findOrCreate({
+        where: { title: tag.slice(1).toLowerCase() },
+      })));
+      await post.addHashtags(result.map(r => r[0]));
     }
-})
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
-router.get('/hashtag',async(req,res,next)=>{
-    const query = req.query.hashtag;
-    if(!query){
-        return res.redirect('/');
-
+router.get('/hashtag', async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
     }
-    try{
-        const hashtag = await Hashtag.findOne({where:{title:query}});
-        let posts = [];
-        if(hashtag){
-            posts = await hashtag.getPosts({include:[{model:User}]});
+    return res.render('main', {
+      title: `${query} | NodeBird`,
+      user: req.user,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
-        }
-        return res.render('main',{title:`${query} | nodebird`,
-                                user:req.user,
-                                twits:posts});
-                            
-    }catch(err){
-        console.error(err);
-        return next(err);
-        
-    }
-})
 module.exports = router;
